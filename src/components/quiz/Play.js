5aa0724b0f105b52b99e647f4ad59b257e711f66
Dileepa Samarawickrama <dansamarawicky@outlook.com>
2020-07-14 11:10:17 +0530

    correct and wrong answer checking functions added

--- src/components/quiz/Play.js
+++ src/components/quiz/Play.js
@@ -1,12 +1,108 @@
 import React, { Component, Fragment } from "react";
 import Helmet from "react-helmet";
+import M from "materialize-css";
+
+import questions from "../../questions.json";
+import isEmpty from "../../utils/is-empty";
 
 class Play extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      questions: questions,
+      currentQuestion: {},
+      nextQuestion: {},
+      previousQuestion: {},
+      answer: "",
+      numberOfQuestions: 0,
+      numberOfAnsweredQuestions: 0,
+      currentQuestionIndex: 0,
+      score: 0,
+      correctAnswers: 0,
+      wrongAnswers: 0,
+      hints: 5,
+      fiftyFifty: 2,
+      usedfiftyFifty: false,
+      time: {},
+    };
   }
 
+  componentDidMount() {
+    const {
+      questions,
+      currentQuestion,
+      nextQuestion,
+      previousQuestion,
+    } = this.state;
+    this.displayQuestions(
+      questions,
+      currentQuestion,
+      nextQuestion,
+      previousQuestion
+    );
+  }
+
+  displayQuestions = (
+    questions = this.state.questions,
+    currentQuestion,
+    nextQuestion,
+    previousQuestion
+  ) => {
+    let { currentQuestionIndex } = this.state;
+    if (!isEmpty(this.state.questions)) {
+      questions = this.state.questions;
+      currentQuestion = questions[currentQuestionIndex];
+      nextQuestion = questions[currentQuestionIndex + 1];
+      previousQuestion = questions[currentQuestionIndex - 1];
+      const answer = currentQuestion.answer;
+      this.setState({
+        currentQuestion,
+        nextQuestion,
+        previousQuestion,
+        answer,
+      });
+    }
+  };
+
+  handleOptionClick = (e) => {
+    if (e.target.innerHTML.toLowerCase() === this.state.answer.toLowerCase()) {
+      this.correctAnswer();
+    } else {
+      this.wrongAnswer();
+    }
+  };
+
+  correctAnswer = () => {
+    M.toast({
+      html: "Correct Answer!",
+      classes: "toast-valid",
+      displayLength: 1500,
+    });
+    this.setState((previousState) => ({
+      score: previousState.score + 1,
+      correctAnswers: previousState.correctAnswers + 1,
+      currentQuestionIndex: previousState.currentQuestionIndex + 1,
+      numberOfAnsweredQuestions: previousState.numberOfAnsweredQuestions + 1,
+    }));
+  };
+
+  wrongAnswer = () => {
+    navigator.vibrate(1000);
+    M.toast({
+      html: "Wrong Answer!",
+      classes: "toast-invalid ",
+      displayLength: 1500,
+    });
+    this.setState((previousState) => ({
+      wrongAnswers: previousState.wrongAnswers + 1,
+      currentQuestionIndex: previousState.currentQuestionIndex + 1,
+      numberOfAnsweredQuestions: previousState.numberOfAnsweredQuestions + 1,
+    }));
+  };
+
   render() {
+    //console.log(questions);
+    const { currentQuestion } = this.state;
     return (
       <Fragment>
         <Helmet>
@@ -37,14 +133,22 @@ class Play extends Component {
             </p>
           </div>
 
-          <h5>What is HTML Stands for?</h5>
+          <h5>{currentQuestion.question}</h5>
           <div className="options-container">
-            <p className="option">Hi Tech Media Library</p>
-            <p className="option">Hypo Text Markup Language</p>
+            <p onClick={this.handleOptionClick} className="option">
+              {currentQuestion.optionA}
+            </p>
+            <p onClick={this.handleOptionClick} className="option">
+              {currentQuestion.optionB}
+            </p>
           </div>
           <div className="options-container">
-            <p className="option">Hyper Text Markup Language</p>
-            <p className="option">Hyper Tags Markup Language</p>
+            <p onClick={this.handleOptionClick} className="option">
+              {currentQuestion.optionC}
+            </p>
+            <p onClick={this.handleOptionClick} className="option">
+              {currentQuestion.optionD}
+            </p>
           </div>
 
           <div className="bottom-container">
