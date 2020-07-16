7550b699b5cfd7d78c07bea1ec73b6d5a94eb598
Dileepa Samarawickrama <dansamarawicky@outlook.com>
2020-07-16 11:03:37 +0530

    added some scss to QuizSummary component, it still not resposive the way it should be

--- src/components/quiz/QuizSummary.js
+++ src/components/quiz/QuizSummary.js
@@ -61,43 +61,50 @@ class QuizSummary extends Component {
       console.log(state.numberOfQuestions);
       stats = (
         <Fragment>
-          <div>
-            <span className="mdi mdi-check-circle-outline mdi-48px success-icon"></span>
+          <div className="summary">
+            <div className="success-icon">
+              <span className="mdi mdi-check-circle-outline "></span>
+            </div>
+            <h1>Quiz has ended!</h1>
+            <div className="container">
+              <h4>{remark}</h4>
+              <h2>Your score : {this.state.score.toFixed(0)}&#37;</h2>
+              <span className="stat left">Total number of questions: </span>
+              <span className="stat right">{state.numberOfQuestions}</span>
+              <br></br>
+              <span className="stat left">Number of attempted questions: </span>
+              <span className="stat right">
+                {state.numberOfAnsweredQuestions}
+              </span>
+              <br></br>
+              <span className="stat left">Number of correct answers: </span>
+              <span className="stat right">{state.correctAnswers}</span>
+              <br></br>
+              <span className="stat left">Number wrong answers: </span>
+              <span className="stat right">{state.wrongAnswers}</span>
+              <br></br>
+              <span className="stat left">Hints Used: </span>
+              <span className="stat right">{state.hintsUsed}</span>
+              <br></br>
+              <span className="stat left">50-50 used: </span>
+              <span className="stat right">{state.fiftyFiftyUsed}</span>
+            </div>
+            <section></section>
+            <div className="buttons-container">
+              <ul>
+                <li>
+                  <Link className="left" to="/">
+                    Back to home
+                  </Link>
+                </li>
+                <li>
+                  <Link className="right" to="/play/quiz">
+                    Play again
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
-          <h1>Quiz has ended!</h1>
-          <div className="container">
-            <h4>{remark}</h4>
-            <h2>Your score : {this.state.score.toFixed(0)}&#37;</h2>
-            <span className="stat left">Total number of questions: </span>
-            <span className="stat right">{state.numberOfQuestions}</span>
-            <br></br>
-            <span className="stat left">Number of attempted questions: </span>
-            <span className="stat right">
-              {state.numberOfAnsweredQuestions}
-            </span>
-            <br></br>
-            <span className="stat left">Number of correct answers: </span>
-            <span className="stat right">{state.correctAnswers}</span>
-            <br></br>
-            <span className="stat left">Number wrong answers: </span>
-            <span className="stat right">{state.wrongAnswers}</span>
-            <br></br>
-            <span className="stat left">Hints Used: </span>
-            <span className="stat right">{state.hintsUsed}</span>
-            <br></br>
-            <span className="stat left">50-50 used: </span>
-            <span className="stat right">{state.fiftyFiftyUsed}</span>
-          </div>
-          <section>
-            <ul>
-              <li>
-                <Link to="/">Back to home</Link>
-              </li>
-              <li>
-                <Link to="/play/quiz">Play again</Link>
-              </li>
-            </ul>
-          </section>
         </Fragment>
       );
     } else {
